7ef36c5294dbeb4dd06bc53220d917bdadd96bd5
irfan378 <irfandominate@gmail.com>
2022-08-27 17:56:48 +0530

    frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import Header from './components/Header';
+import React from "react";
+import Header from "./components/Header";
+import { ApolloProvider, InMemoryCache, ApolloClient } from "@apollo/client";
+import Client from "./components/Client";
+
+const client=new ApolloClient({
+  uri:"http://localhost:5000/graphql",
+  cache:new InMemoryCache()
+})
 
 function App() {
   return (
     <>
-  <Header/>
+    <ApolloProvider client={client}>
+      <Header />
+      <Client/>
+      </ApolloProvider>
     </>
   );
 }
