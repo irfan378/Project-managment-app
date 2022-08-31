30e06be8f4c51ab1a8ddc9ad287dda7bd755ce1a
irfan378 <irfandominate@gmail.com>
2022-08-31 18:35:33 +0530

    Add Client

--- client/src/components/AddClientModal.jsx
+++ client/src/components/AddClientModal.jsx
@@ -1,25 +1,80 @@
 import React from "react";
 import { useState } from "react";
-const AddClientModal = () => {
-  const [open, setOpen] = useState(false);
+import "./AddClientModel.css";
+import { ADD_CLIENT } from "../mutation/clientMutation";
+import { GET_CLIENTS } from "../queries/clientQueries";
+import { useMutation } from "@apollo/client";
+const AddClientModal = ({ open, setOpen }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [addClient] = useMutation(ADD_CLIENT, {
+    variables: { name, email, phone },
+    update(cache, { data: { addClient } }) {
+      const { clients } = cache.readQuery({
+        query: GET_CLIENTS,
+      });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: { clients: [...clients,addClient] },
+      });
+    },
+  });
   const openModel = () => {
     setOpen(true);
   };
-  const closeModel=()=>{
+  const closeModel = () => {
+    setOpen(false);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name==='' ||email===''||phone==='') {
+      return alert('Please fill in all Fields')
+    }
+    addClient(name,email,phone);
+    setName('')
+    setEmail('')
+    setPhone('')
     setOpen(false)
-  }
+  };
   return (
-    <div>
+    <div className="modal">
       {open ? (
-        <div id="myModal" class="modal">
-          <div class="modal-content">
-            <span onClick={closeModel} class="close">&times;</span>
-            <p>Some text in the Modal..</p>
-          </div>
+        <div id="myModal">
+         
+            <span onClick={closeModel} className="close">
+              &times;
+            </span>
+            <form onSubmit={handleSubmit}>
+            <div className="modal-content">
+            <input
+              type="text"
+              value={name}
+              placeholder="Name"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+            <button id="myBtn" type="submit" className="btn">
+              Add
+            </button>
+            </div>
+            </form>
+      
         </div>
       ) : (
         <button onClick={openModel} id="myBtn">
-          Open Modal
+          Add Client
         </button>
       )}
     </div>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,21 @@
-import "./Header.css"
+import "./Header.css";
 import React from "react";
-import logo from "./assets/logo.jpg"
+import logo from "./assets/logo.jpg";
 import AddClientModal from "./AddClientModal";
+import { useState } from "react";
 const Header = () => {
-  return(
-  <nav className="navbar">
-<div className="nav">
-    <img src={logo} alt="" width="30px" height="30px" />
-</div>
-    <div className="nav">Project Managment 
-    </div>
-    <AddClientModal/>  
-  </nav>
-  )
+  const [open, setOpen] = useState(false);
+  return (
+    <nav className="navbar">
+      <div className="nav">
+        <img src={logo} alt="" width="30px" height="30px" />
+      </div>
+      <div className="nav">Project Managment</div>
+      <div className="btn">
+          <AddClientModal open={open} setOpen={setOpen} />
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
